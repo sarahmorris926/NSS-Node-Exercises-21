b3f02f008807b756a36088be178c74e8d538b129
Sarah Morris <sarah.morris9@gmail.com>
2018-03-18 18:45:44 -0500

    Completed exercise

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
  
 const express = require('express');
 const app = express();
-require('dotenv').config();
-const { printEgg } = require('./egg.js');
+const middleware = require('./egg.js');
+
 
 // middleware
-
-app.get('/', (req, res) => {
-    res.send(`Welcome to Chickens 'n Eggz!`);
-  })
+app.use('/farm', middleware)
   
-  app.use('./public/eggs', printEgg);
-  
-  app.use(express.static('/public', { extensions: 'html' }));
 
 // error handling
 app.use((req, res, next) => {
@@ -21,15 +15,13 @@ app.use((req, res, next) => {
     next(err);
 })
 
-// custom error handler at bottom
 app.use( (err, req, res, next) => {
     res.status(err.status || 500)
-    res.json({
-        message: "Error error error!",
-        err: err.message
-    });
+    res.send('<h1>No farm animals here, try again!</h1>')
 });
 
+
+// server stuff
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Listening on port ', port);
--- egg.js
+++ egg.js
@@ -1,24 +1,48 @@
+const {Router} = require('express');
+const farmRouter = Router();
 
-module.exports.printEgg = (res, req, next) => {
-  let date = new Date;
-  console.log(`You found the Easter Egg at ${date.toLocaleDateString('en-us')}`);
-  console.log(`
-        ,ggadddd8888888bbbbaaa,_
+const printEgg = () => {
+  let egg =
+  `       ,ggadddd8888888bbbbaaa,_
      ,ad888,      \`Y88,      \`Y888baa,
    ,dP"  "Y8b,      \`"Y8b,      \`"Y8888ba,
-  ,88      "Y88b,      \`"Y8ba,       \`"Y88Ya,
- ,P88b,      \`"Y88b,       \`"Y8ba,_       ""8a,
-,P'"Y88b,        "Y88b,        \`"Y8ba,_      \`Ya,
-8'    "Y88b,        ""Y8ba,         ""Y8ba,_   \`8,
-8b       "Y88b,         ""Y8ba,_         ""Y88baaY
-88b,        "Y88ba,         ""Y88ba,_         \`""P
-8Y88ba,        ""Y88ba,_         ""Y88ba,,_    ,P'
-\`b,"Y88ba,         ""Y88baa,_         """Y888bd"
+  , 88      "Y88b,      \`"Y8ba, \`"Y88Ya,
+ ,P88b,      \`"Y88b,       \`"Y8ba, _       ""8a,
+, P'"Y88b,        "Y88b,        \`"Y8ba,_      \`Ya,
+  8'    "Y88b,        ""Y8ba,         ""Y8ba,_   \`8,
+  8b       "Y88b,         ""Y8ba,_         ""Y88baaY
+  88b, "Y88ba,         ""Y88ba,_         \`""P
+  8Y88ba, ""Y88ba, _         ""Y88ba,, _, P'
+    \`b,"Y88ba,         ""Y88baa,_         """Y888bd"
  \`b, \`"Y88ba,_         ""Y888baa,_         ,8"
-  \`8,   \`""Y88ba,_         \`"""Y8888baaaaaP"
-   \`Ya,     \`""Y888ba,_           \`"d88P"
+  \`8, \`""Y88ba,_         \`"""Y8888baaaaaP"
+    \`Ya,     \`""Y888ba, _\`"d88P"
      \`"Yb,,_     \`""Y888baa,__,,adP""'
-         \`"""YYYY8888888PPPP"""'
-  `);
-  next();
+         \`"""YYYY8888888PPPP"""'                    `;
+
+  return egg;
 };
+
+const printTime = () => {
+  let date = new Date;
+  let eggString = `You found the Easter Egg at ${date.toLocaleDateString('en-us')}`;
+  return eggString;
+}
+
+farmRouter.get("/chickens", (req, res, next) => {
+  res.sendFile(__dirname + '/public/see-our-chickens.html');
+});
+
+farmRouter.get("/eggs", (req, res, next) => {
+  let printString = printTime();
+  let printEasterEgg = printEgg();
+  console.log(printString);
+  console.log(printEasterEgg);
+  res.sendFile(__dirname + '/public/see-our-eggs.html');
+});
+
+farmRouter.get('/home', (req, res, next) => {
+  res.sendFile(__dirname + '/public/home.html');
+})
+
+module.exports = farmRouter;
